aba04c71fd4dc1a9c83b9f3dc77d9294ae349077
aspatil919 <68045526+aspatil919@users.noreply.github.com>
2021-10-20 21:49:44 +0530

    Update variables.tf

--- Demo/variables.tf
+++ Demo/variables.tf
@@ -1,1 +1,33 @@
 
+variable "AZURE_TENANT_ID" {
+  type = string
+}
+
+variable "AZURE_SUBSCRIPTION_ID" {
+  type = string
+}
+
+variable "AZURE_CLIENT_ID" {
+  type = string
+}
+
+variable "AZURE_CLIENT_SECRET" {
+  type      = string
+  sensitive = true
+}
+
+variable "HCA_COST_ID" {
+  type = string
+}
+
+variable "HCA_COST_CENTER" {
+  type = string
+}
+
+variable "HCA_DEPARTMENT_ID" {
+  type = string
+}
+
+variable "HCA_APP_CODE" {
+  type = string
+}
